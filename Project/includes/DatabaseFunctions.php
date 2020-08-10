f29fef81e2ec4b87731fb8c9457920fdd8a164b0
Paolo Vagnini <paolondon@gmail.com>
2020-08-10 15:45:48 +0200

    refactor creata la funzione query con param

--- Project/includes/DatabaseFunctions.php
+++ Project/includes/DatabaseFunctions.php
@@ -3,17 +3,25 @@
 function totalJokes($pdo)
 {
     $sql = 'SELECT COUNT(*) FROM `joke`';
-    $query = $pdo->prepare($sql);
-    $query->execute();
+    $query = query($pdo, $sql);
     return $query->fetch()[0];
 }
 
-function getJoke($pdo, $jokeId)
+function getJoke($pdo, $id)
 {
-    $sql = 'SELECT * FROM `joke` WHERE `id` = :jokeid';
-    $query = $pdo->prepare($sql);
-    $query->bindValue(':jokeid', $jokeId);
-    $query->execute();
+    $sql = 'SELECT * FROM `joke` WHERE `id` = :id';
+    $parameters = [':id' => $id];
+    $query = query($pdo, $sql, $parameters);
     $row = $query->fetch();
     return $row;
+}
+
+function query($pdo, $sql, $parameters = [])
+{
+    $query = $pdo->prepare($sql);
+    foreach ($parameters as $key => $value) {
+        $query->bindValue($key, $value);
+    }
+    $query->execute();
+    return $query;
 }
