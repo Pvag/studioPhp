c1f57a8d5fec5cf394e33a6073e37107d8c2de8f
Paolo Vagnini <paolondon@gmail.com>
2020-08-12 13:57:21 +0200

    refactor remove addjoke and template

--- Project/templates/addjoke.html.php
+++
@@ -1,5 +0,0 @@
-<form action="" method="POST">
-    <label for="joketext">Insert the text of your joke:</label>
-    <textarea name="joketext" cols="30" rows="10"></textarea>
-    <input type="submit" value="Submit!">
-</form>
